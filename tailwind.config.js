f6870744242b354bcf73ad5cc2b84ce696bda9ae
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2023-06-08 21:41:34 +0700

    move public dir contents to root

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import { fontFamily } from "tailwindcss/defaultTheme.js";
 
 /** @type { import('tailwindcss').Config } */
 export default {
-	content: ["./public/**/*.{html,js}"],
+	content: ["./**/*.{html,js}"],
 	theme: {
 		fontFamily: {
 			display: ["Poppins"],
